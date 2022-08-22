52c56538cef8e6d05a580b4baf3c6d0507e31b22
Biggunlotsoffun <gberg3@huskers.unl.edu>
2022-08-22 02:10:49 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * basic hello world program
+ */
+
+init main(int argc, chr **argv) {
+
+    print("hello my name is Gunnar Berg! \n");
+    print("Major is Computer Engineering! \n");
+
+    return 0;
+}
